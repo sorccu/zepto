0eb972c145f5799db6851b115a30049e7f6e2079
Simo Kinnunen <simo@shoqolate.com>
2012-09-20 10:12:11 +0900

    simulate touch events via mouse events if native support is not available

--- src/touch.js
+++ src/touch.js
@@ -5,7 +5,8 @@
 ;(function($){
   var touch = {},
     touchTimeout, tapTimeout, swipeTimeout,
-    longTapDelay = 750, longTapTimeout
+    longTapDelay = 750, longTapTimeout,
+    simulate = !('ontouchstart' in document)
 
   function parentIfText(node) {
     return 'tagName' in node ? node : node.parentNode
@@ -38,71 +39,89 @@
     touch = {}
   }
 
-  $(document).ready(function(){
+  function handleStart(localTouch, target) {
     var now, delta
+    now = Date.now()
+    delta = now - (touch.last || now)
+    touch.el = $(parentIfText(target))
+    touchTimeout && clearTimeout(touchTimeout)
+    touch.x1 = localTouch.pageX
+    touch.y1 = localTouch.pageY
+    if (delta > 0 && delta <= 250) touch.isDoubleTap = true
+    touch.last = now
+    longTapTimeout = setTimeout(longTap, longTapDelay)
+  }
 
-    $(document.body)
-      .bind('touchstart', function(e){
-        now = Date.now()
-        delta = now - (touch.last || now)
-        touch.el = $(parentIfText(e.touches[0].target))
-        touchTimeout && clearTimeout(touchTimeout)
-        touch.x1 = e.touches[0].pageX
-        touch.y1 = e.touches[0].pageY
-        if (delta > 0 && delta <= 250) touch.isDoubleTap = true
-        touch.last = now
-        longTapTimeout = setTimeout(longTap, longTapDelay)
-      })
-      .bind('touchmove', function(e){
-        cancelLongTap()
-        touch.x2 = e.touches[0].pageX
-        touch.y2 = e.touches[0].pageY
-      })
-      .bind('touchend', function(e){
-         cancelLongTap()
+  function handleMove(localTouch) {
+    cancelLongTap()
+    touch.x2 = localTouch.pageX
+    touch.y2 = localTouch.pageY
+  }
 
-        // swipe
-        if ((touch.x2 && Math.abs(touch.x1 - touch.x2) > 30) ||
-            (touch.y2 && Math.abs(touch.y1 - touch.y2) > 30))
+  function handleEnd() {
+    cancelLongTap()
 
-          swipeTimeout = setTimeout(function() {
-            touch.el.trigger('swipe')
-            touch.el.trigger('swipe' + (swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2)))
+    // swipe
+    if ((touch.x2 && Math.abs(touch.x1 - touch.x2) > 30) ||
+        (touch.y2 && Math.abs(touch.y1 - touch.y2) > 30))
+
+      swipeTimeout = setTimeout(function() {
+        touch.el.trigger('swipe')
+        touch.el.trigger('swipe' + (swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2)))
+        touch = {}
+      }, 0)
+
+    // normal tap
+    else if ('last' in touch)
+
+      // delay by one tick so we can cancel the 'tap' event if 'scroll' fires
+      // ('tap' fires before 'scroll')
+      tapTimeout = setTimeout(function() {
+
+        // trigger universal 'tap' with the option to cancelTouch()
+        // (cancelTouch cancels processing of single vs double taps for faster 'tap' response)
+        var event = $.Event('tap')
+        event.cancelTouch = cancelAll
+        touch.el.trigger(event)
+
+        // trigger double tap immediately
+        if (touch.isDoubleTap) {
+          touch.el.trigger('doubleTap')
+          touch = {}
+        }
+
+        // trigger single tap after 250ms of inactivity
+        else {
+          touchTimeout = setTimeout(function(){
+            touchTimeout = null
+            touch.el.trigger('singleTap')
             touch = {}
-          }, 0)
+          }, 250)
+        }
 
-        // normal tap
-        else if ('last' in touch)
+      }, 0)
+  }
 
-          // delay by one tick so we can cancel the 'tap' event if 'scroll' fires
-          // ('tap' fires before 'scroll')
-          tapTimeout = setTimeout(function() {
+  $(document).ready(function(){
 
-            // trigger universal 'tap' with the option to cancelTouch()
-            // (cancelTouch cancels processing of single vs double taps for faster 'tap' response)
-            var event = $.Event('tap')
-            event.cancelTouch = cancelAll
-            touch.el.trigger(event)
-
-            // trigger double tap immediately
-            if (touch.isDoubleTap) {
-              touch.el.trigger('doubleTap')
-              touch = {}
-            }
-
-            // trigger single tap after 250ms of inactivity
-            else {
-              touchTimeout = setTimeout(function(){
-                touchTimeout = null
-                touch.el.trigger('singleTap')
-                touch = {}
-              }, 250)
-            }
-
-          }, 0)
-
+    $(document.body).bind(simulate ? {
+        mousedown: function(e) {
+          handleStart(e, e.target)
+        },
+        mousemove: function(e) {
+          handleMove(e)
+        },
+        mouseup: handleEnd
+      } : {
+        touchstart: function(e){
+          handleStart(e.touches[0], e.target)
+        },
+        touchmove: function(e){
+          handleMove(e.touches[0])
+        },
+        touchend: handleEnd,
+        touchcancel: cancelAll
       })
-      .bind('touchcancel', cancelAll)
 
     $(window).bind('scroll', cancelAll)
   })
